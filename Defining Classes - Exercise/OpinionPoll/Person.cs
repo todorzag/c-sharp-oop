7b3586d021942f87c1b270cb4f93718b107e07d3
todorzag <todorflorida20@gmail.com>
2022-08-01 16:21:27 +0300

    Added OpinionPoll

---
+++ Defining Classes - Exercise/OpinionPoll/Person.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpinionPoll
+{
+    internal class Person
+    {
+        private string name;
+        private int age;
+
+        public string Name { get { return name; } set { name = value; } }
+
+        public int Age { get { return age; } set { age = value; } }
+
+        public Person()
+        {
+            Name = "No name";
+            Age = 1;
+        }
+
+        public Person(int age) : this()
+        {
+            Age = age;
+        }
+
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age} ";
+        }
+    }
+}
---
+++ Defining Classes - Exercise/OpinionPoll/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace OpinionPoll
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] split = Console.ReadLine().Split(" ");
+                string name = split[0];
+                int age = int.Parse(split[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            List<Person> peopleOver30 = family.GetAllPeopleOver30();
+            List<Person> orderedList = SortPeopleByName(peopleOver30);
+
+            PrintPeople(orderedList);
+        }
+
+        public static List<Person> SortPeopleByName(List<Person> people)
+        {
+            return people.OrderBy(person => person.Name).ToList();
+        }
+
+        public static void PrintPeople(List<Person> people)
+        {
+            foreach (Person person in people)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
