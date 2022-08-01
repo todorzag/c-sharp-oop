4465d34c610d8e9b34e798c4af67b25b7d6a4278
todorzag <todorflorida20@gmail.com>
2022-08-01 17:10:00 +0300

    Added SpeedRacing

---
+++ Defining Classes - Exercise/SpeedRacing/Car.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpeedRacing
+{
+    internal class Car
+    {
+        public string Model { get; set; }
+
+        public double FuelAmount { get; set; }
+
+        public double FuelConsumptionPerKilometer { get; set; }
+
+        private double TravelledDistance { get; set; }
+
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            Model = model;
+            FuelAmount = fuelAmount;
+            FuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+        }
+
+        public void Drive(double distanceToDrive)
+        {
+            double fuelNeeded = FuelConsumptionPerKilometer * distanceToDrive;
+
+            if (FuelAmount >= fuelNeeded)
+            {
+                FuelAmount -= fuelNeeded;
+                TravelledDistance += distanceToDrive;
+            }
+            else
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{Model} {FuelAmount:0.00} {TravelledDistance}";
+        }
+    }
+}
