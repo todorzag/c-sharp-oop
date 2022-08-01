fcb0ab56c0704bc88c5b8ddfdc539aad5e51daea
todorzag <todorflorida20@gmail.com>
2022-08-01 15:54:18 +0300

    Added OldestFamilyMember

---
+++ Defining Classes - Exercise/OldestFamilyMember/Family.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OldestFamilyMember
+{
+    internal class Family
+    {
+        private List<Person> People { get; set; }
+
+        public Family()
+        {
+            People = new List<Person>();
+        }
+
+        public void AddMember(Person member)
+        {
+            People.Add(member);
+        }
+
+        public Person GetOldestMember()
+        {
+            return People.OrderByDescending(m => m.Age).ToArray()[0];
+        }
+    }
+}
---
+++ Defining Classes - Exercise/OldestFamilyMember/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace OldestFamilyMember
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfPeople = int.Parse(Console.ReadLine());
+
+            Family family = new Family();
+
+            for (int i = 0; i < numberOfPeople; i++)
+            {
+                string[] split = Console.ReadLine().Split(" ");
+                string name = split[0];
+                int age = int.Parse(split[1]);
+
+                family.AddMember(new Person(name, age));
+            }
+
+            Person oldestFamilyMember = family.GetOldestMember();
+
+            Console.WriteLine(oldestFamilyMember.WhoAmI());
+        }
+    }
+}
