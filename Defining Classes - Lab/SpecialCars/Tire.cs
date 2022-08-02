32a8282b91a969fc8db37810d3021a9b46dec1b3
Todor Zagorov <todorflorida20@gmail.com>
2022-08-02 11:40:56 +0300

    Fixed Previous Issues

--- Defining Classes - Lab/SpecialCars/Tire.cs
+++ Defining Classes - Lab/SpecialCars/Tire.cs
@@ -8,17 +8,14 @@ namespace CarAndEngine
 {
     internal class Tire
     {
-        private int year;
-        private double pressure;
+        public int Year { get; set; }
 
-        public int Year { get { return year; } set { year = value; } }
-
-        public double Pressure { get { return pressure; } set { pressure = value; } }
+        public double Pressure { get; set; }
 
         public Tire(int year, double pressure)
         {
-            this.year = year;
-            this.pressure = pressure;
+            Year = year;
+            Pressure = pressure;
         }
     }
 }
