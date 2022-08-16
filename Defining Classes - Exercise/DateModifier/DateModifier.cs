86ee135edcf293ee8ccfb08b5056b44a901d926d
Todor Zagorov <todorflorida20@gmail.com>
2022-08-16 16:28:14 +0300

    Removed ReturnDateTimeFromString Method instead used DateTime.Parse()

--- Defining Classes - Exercise/DateModifier/DateModifier.cs
+++ Defining Classes - Exercise/DateModifier/DateModifier.cs
@@ -14,23 +14,12 @@ namespace DateModifier
 
         public void GetDateDifference(string date1, string date2)
         {
-            DateTime dateTime1 = ReturnDateTimeFromString(date1);
-            DateTime dateTime2 = ReturnDateTimeFromString(date2);
+            DateTime dateTime1 = DateTime.Parse(date1);
+            DateTime dateTime2 = DateTime.Parse(date2);
 
             TimeSpan diff = dateTime2.Subtract(dateTime1);
 
             DayDifference = Math.Abs(diff.Days);
-        }
-
-        public DateTime ReturnDateTimeFromString(string date)
-        {
-            string[] split = date.Split(" ");
-
-            int year = int.Parse(split[0]);
-            int month = int.Parse(split[1]);
-            int day = int.Parse(split[2]);
-
-            return new DateTime(year, month, day);
         }
     }
 }
