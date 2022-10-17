51f1a3bde431bebc5bad1c424d716e56a8b1b9f8
Todor Zagorov <todorflorida20@gmail.com>
2022-10-17 16:15:16 +0300

    Added previously unsolved tasks

--- SQL/DB Modification Tasks/Solutions/13/PCQuery.sql
+++ SQL/DB Modification Tasks/Solutions/13/PCQuery.sql
@@ -10,6 +10,30 @@ WHERE maker IN (
 )
 AND type = 'printer'
 
+WITH CTE_AVG AS (
+	SELECT maker, AVG(price) AS AVG_PRICE
+	FROM product
+	JOIN printer ON printer.model = product.model
+	GROUP BY maker
+), CTE_MODELS AS (
+	SELECT model
+	FROM product
+	WHERE type = 'PC'
+	AND maker IN (
+		SELECT maker 
+		FROM CTE_AVG
+		WHERE AVG_PRICE > 800
+	)
+)
+
+UPDATE pc
+SET price -= (price * 0.05)
+FROM pc
+WHERE model IN (
+	SELECT model
+	FROM CTE_MODELS
+)
+
 SELECT HD, MIN(PRICE)
 FROM PC
 WHERE HD<30 AND HD>10
